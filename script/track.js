51fe37b955ed70c50c4558a764100ac42ec06fd4
Aung Myo Aye <155707551+AungMyoAyeDev@users.noreply.github.com>
2024-01-19 22:17:31 +0630

    Add files via upload
    Adding data for app

---
+++ script/track.js
@@ -0,0 +1,98 @@
+export const tracks = [
+    {
+        title: "Greedy",
+        artist: "Tate McRae",
+        album: "greedy",
+        src: "./music/greedy.m4a",
+        id: "sng-1",
+        img: "./img/greedy.jpg"
+    },
+    {
+        title: "Love is Gone",
+        artist: "Slander",
+        album: "love is gone",
+        src: "./music/loveisgone.m4a",
+        id: "sng-2",
+        img: "./img/loveisgone.jpg"
+    },
+    {
+        title: "Makeba",
+        artist: "Jain",
+        album: "Zanaka",
+        src: "./music/makiba.m4a",
+        id: "sng-3",
+        img: "./img/makiba.jpeg"
+    },
+    {
+        title: "My stupid heart",
+        artist: "Luminati Suns,Walk off the Earth",
+        album: "Stand By you",
+        src: "./music/mystupidheart.m4a",
+        id: "sng-4",
+        img: "./img/mystupidheart.jpeg"
+    },
+    {
+        title: "Navada",
+        artist: "Vicetone",
+        album: "Navada",
+        src: "./music/navada.m4a",
+        id: "sng-5",
+        img: "./img/navada.jpg"
+    },
+    {
+        title: "Touch My body",
+        artist: "Mariah Carey",
+        album: "E=MC²",
+        src: "./music/touchmybody.m4a",
+        id: "sng-6",
+        img: "./img/touchmybody.jpg"
+    },
+    {
+        title: "Back To You",
+        artist: "Selena Gomez",
+        album: "Selena",
+        src: "./music/backtoyou.m4a",
+        id: "sng-7",
+        img: "./img/backtoyou.jpg"
+    },
+    {
+        title: "The Nights",
+        artist: "Avicii",
+        album: "love is gone",
+        src: "./music/thenights.m4a",
+        id: "sng-8",
+        img: "./img/thenights.jpg"
+    },
+    {
+        title: "IDGAF",
+        artist: "Dua Lipa",
+        album: "IDGAF",
+        src: "./music/idgaf.m4a",
+        id: "sng-9",
+        img: "./img/idgaf.jpg"
+    },
+    {
+        title: "Single Soon",
+        artist: "Selena Gomez",
+        album: "Single Soon",
+        src: "./music/singlesoon.m4a",
+        id: "sng-10",
+        img: "./img/singlesoon.jpeg"
+    },
+    {
+        title: "Shape Of You",
+        artist: "Ed Sheeran",
+        album: "Shape of you",
+        src: "./music/shapeofyou.m4a",
+        id: "sng-11",
+        img: "./img/shapeofyou.jpg"
+    },
+    {
+        title: "Just Look Up",
+        artist: "Ariana Grande & Kid Cuidi",
+        album: "Don't Look Up",
+        src: "./music/Justlookup.m4a",
+        id: "sng-12",
+        img: "./img/justlookup.jpeg"
+    }
+];
